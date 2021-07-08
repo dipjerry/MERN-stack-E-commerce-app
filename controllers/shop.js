cc120d0a321ecd069ac70448a927a7e9fdc84b1f
Dipjyoti Kashyap <kaxyapdip@gmail.com>
2021-07-08 22:38:31 +0530

    xxx

--- controllers/shop.js
+++ controllers/shop.js
@@ -137,7 +137,7 @@ exports.getCheckout = (req, res, next) => {
               quantity: p.quantity,
             };
           }),
-          success_url: req.protocol + '://' + req.get('host') + '/checkout/success',
+          success_url: req.protocol+'://' + req.get('host') + '/checkout/success',
           cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel',
 
         });
@@ -146,7 +146,7 @@ exports.getCheckout = (req, res, next) => {
         console.log('session');
         console.log(session);
         console.log('PASSS');
-        res.render('shop/test', {
+        res.render('shop/checkout', {
           path: '/checkout',
           docTitle: 'checkout',
           hasProduct: products.length > 0,
