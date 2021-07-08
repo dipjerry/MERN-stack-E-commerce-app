8152b0abc3e52531d04de54e827aff1eda1d41ff
Dipjyoti Kashyap <kaxyapdip@gmail.com>
2021-07-08 21:03:48 +0530

    errr

--- controllers/shop.js
+++ controllers/shop.js
@@ -151,6 +151,7 @@ exports.getCheckout = (req, res, next) => {
         });
       })
       .catch((err) => {
+        console.log('err');
         console.log(err);
         const error = new Error(err);
         error.httpStatusCode = 500;
